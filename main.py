8a5057a48a2c6cdd52f6c816d0e272fca4013e42
Jyunmau <yangyang4910709@163.com>
2023-09-24 00:47:09 +0800

    改进特征向量构造方式以使用与指定数学模型一致的一阶多项式范式

--- main.py
+++ main.py
@@ -9,24 +9,41 @@ from matplotlib import pyplot as plt
 
 def do_cal():
     # 定义数据集的起始年份和年份数量
-    start_year = 2
+    start_year = 2011
     num_year = 10
     # 定义区域人口总量用于制图（预测），并转秩为列向量
     P_o = np.array([7869.34, 8022.99, 8119.81, 8192.44, 8281.09, 8315.11, 8381.47, 8423.50, 8446.19, 8469.09, 8477.26])
     P_o = P_o.reshape(-1, 1)
     # 定义区域GDP用于制图（预测），并转秩为列向量
     G_o = np.array([41383.87, 45952.65, 50660.20, 55580.11, 60359.43, 65552.00, 70665.71, 75752.20, 80827.71, 85556.13, 88683.21])
-    G_o = P_o.reshape(-1, 1)
-    # 定义区域人口总量用于训练，因需要匹配增长率的数据个数，该数据比_o少第一位数据，并转秩为列向量
+    G_o = G_o.reshape(-1, 1)
+    # 定义区域人口总量用于训练，因需要匹配增长率的数据个数，该数据比_o少第一位数据，并转秩为列向量，此处可重新定义为所需的训练数据集
     P = np.array(P_o[1:])
-    # 定义区域GDP用于训练，因需要匹配增长率的数据个数，该数据比_o少第一位数据，并转秩为列向量
+    # 定义区域GDP用于训练，因需要匹配增长率的数据个数，该数据比_o少第一位数据，并转秩为列向量，此处可重新定义为所需的训练数据集
     G = np.array(G_o[1:])
 
-    # 构造模型训练的特征矩阵，拼接人口总量与GDP总量的向量，形成一个n行2列的矩阵
-    X = np.concatenate((P, G), axis=1)
-
+    # 由于要构造人口总量与GDP的乘积以及制图标点需要，年份、人口总量和GDP数据的个数必须相等
     len_p = len(P)
     len_g = len(G)
+    if len_p != len_g:
+        print('error: length of P and G should equal!')
+        return
+    elif len_p != num_year:
+        print('error: length of P and num_year should equal!')
+        return
+
+    # 定义人口总量与GDP的乘积向量
+    PG = np.zeros((1, len_p))
+    i = 0
+    while i in range(0, len_p):
+        PG[0][i] = P[i][0] * G[i][0]
+        i = i + 1
+    PG = PG.reshape(-1, 1)
+
+    # 构造人口增长率模型训练的特征矩阵，拼接人口总量、GDP、人口总量与GDP乘积的向量，形成一个n行3列的矩阵
+    X_pt = np.concatenate((P, G, PG), axis=1)
+    # 构造GPD增长率模型训练的特征矩阵，拼接GDP、人口总量与GDP乘积的向量，形成一个n行2列的矩阵
+    X_gt = np.concatenate((G, PG), axis=1)
 
     # 构造dp/dt，即人口增长率，人口总量对时间的一阶导，并转秩为列向量
     Pt = np.zeros((1, len_p))
@@ -45,8 +62,8 @@ def do_cal():
     Gt = Gt.reshape(-1, 1)
 
     # 构造二次多项式模型参数，以下方法中degree控制阶数，include_bias控制是否增加偏置常量
-    model_Pt_poly_features = PolynomialFeatures(degree=2, include_bias=False)
-    model_Gt_poly_features = PolynomialFeatures(degree=2, include_bias=False)
+    model_Pt_poly_features = PolynomialFeatures(degree=1, include_bias=False)
+    model_Gt_poly_features = PolynomialFeatures(degree=1, include_bias=False)
     # 构造人口增长率预测模型，使用logistic回归
     model_Pt = LinearRegression()
     pipline_Pt = Pipeline([('poly_feature', model_Pt_poly_features), ('model', model_Pt)])
@@ -55,25 +72,30 @@ def do_cal():
     pipline_Gt = Pipeline([('poly_feature', model_Gt_poly_features), ('model', model_Gt)])
 
     # 训练
-    pipline_Pt.fit(X, Pt)
-    pipline_Gt.fit(X, Gt)
+    pipline_Pt.fit(X_pt, Pt)
+    pipline_Gt.fit(X_gt, Gt)
 
     # 预测，此处X可以重新构造为预测数据集，此处使用的仍是2010～2020的训练数据集
-    Pt_predict = pipline_Pt.predict(X)
-    Gt_predict = pipline_Gt.predict(X)
+    Pt_predict = pipline_Pt.predict(X_pt)
+    Gt_predict = pipline_Gt.predict(X_gt)
+
     # 用实际增长率与预测增长率计算均方差以评估拟合情况
     mse_Pt = mean_squared_error(Pt, Pt_predict)
+    print('人口增长率预测方差')
     print(mse_Pt)
     mse_Gt = mean_squared_error(Gt, Gt_predict)
+    print('GDP增长率预测方差')
     print(mse_Gt)
 
-    # 打印模型权重和偏置，权重值顺序同标准多项式公式中
+    # 打印模型权重和偏置，权重值顺序同标准多项式公式
+    print('人口增长率模型参数，参考多项式w1*P+W2*G+W3*PG+b，输出权重向量为[w1 w2 w3]，偏置向量为[b]')
     print(model_Pt.coef_)
     print(model_Pt.intercept_)
-    print(model_Pt.coef_)
-    print(model_Pt.intercept_)
+    print('GDP增长率模型参数，参考多项式W1*G+W2*PG+b，输出权重向量为[w1 w2]，偏置向量为[b]')
+    print(model_Gt.coef_)
+    print(model_Gt.intercept_)
 
-    # 构造时间坐标轴
+    # 构造制图用的时间坐标轴
     t = np.linspace(0, 1, num_year)
     i = 0
     while i in range(0, num_year):
@@ -83,12 +105,14 @@ def do_cal():
     # 打印制图，横轴是时间，纵轴是人口总量或GDP
     plt.figure()
     plt.title("population")
-    plt.scatter(t, (1+Pt)*P, marker='o', c='r')
-    plt.plot(t, (1+Pt_predict)*P, 'g-')
+    plt.scatter(t, (1 + Pt) * P_o[:-1], marker='o', c='r')
+    plt.plot(t, (1 + Pt_predict) * P_o[:-1], 'g-')
+    plt.draw()
     plt.figure()
     plt.title("GDP")
-    plt.scatter(t, (1 + Gt) * G, marker='o', c='r')
-    plt.plot(t, (1 + Gt_predict) * G, 'g-')
+    plt.scatter(t, (1 + Gt) * G_o[:-1], marker='o', c='r')
+    plt.plot(t, (1 + Gt_predict) * G_o[:-1], 'g-')
+    plt.draw()
     plt.show()
 
 
